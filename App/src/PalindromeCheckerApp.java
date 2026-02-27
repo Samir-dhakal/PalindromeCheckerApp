fcf96df62e95d28364235256a107389db57a32a6
Samir-dhakal <samirdhakal.9279@gmail.com>
2026-02-27 15:47:21 +0530

    UC2 hardcoded parlindrome checking app

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
     /**
@@ -14,6 +16,16 @@ public class PalindromeCheckerApp {
         System.out.println("welcome to palindrom Checker Management System");
         System.out.println("version :1.0");
         System.out.println("System initialized successfully ");
+      Scanner input = new Scanner(System.in);
+
+        String str=new String("hafrdh");
+        for( int i =0;i<str.length()/2;i++){
+            if(str.charAt(i)!=str.charAt(str.length()-1-i))
+            {  System.out.println("The string  not the plindrome");
+                return ;}
+        }
+        System.out.println(" the string is palindrome ");
+
 
     }
 }
