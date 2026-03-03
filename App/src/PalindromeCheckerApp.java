d4ea74115f02a584411e9e86bc67fd72624d8a6b
samir-dhakal <samirdhakal.9279@gmail.com>
2026-03-03 23:40:36 +0530

    UC11 completed

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,9 +1,19 @@
 import java.util.LinkedList;
 import java.util.List;
 
-
+ class PalindrormeChecker{
+    public static boolean checkPalindrome(String str){
+        str=str.trim().toLowerCase();
+        for(int i=0;i<str.length()/2;i++){
+            if(str.charAt(i)!=str.charAt(str.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
 public class PalindromeCheckerApp {
-
+PalindrormeChecker obj = new PalindrormeChecker();
     /**
      * Application entry point
      * This is the first method executed by the JVM
@@ -14,21 +24,13 @@ public class PalindromeCheckerApp {
      * @version 1.0
      * UC10: Case-Insensitive & Space-Ignored Palindrome
      */
-    public static boolean isPalindrome(String str) {
-      str=str.trim().toLowerCase();
-      for(int i=0;i<str.length()/2;i++){
-          if(str.charAt(i)!=str.charAt(str.length()-i-1)){
-              return false;
-          }
-      }
-      return true;
-    }
+
     public static void main(String[] args) {
 
 
         String orginalString = "kalilak";
 
-        System.out.println(" The given string " + orginalString + " is palindrome :"+isPalindrome(orginalString));
+        System.out.println(" The given string " + orginalString + " is palindrome :"+ PalindrormeChecker.checkPalindrome(orginalString));
 
     }
 }
