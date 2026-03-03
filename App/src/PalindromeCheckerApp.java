8e4f2ca180a78a518f55cf0af71c82f412464a8f
samir-dhakal <samirdhakal.9279@gmail.com>
2026-03-03 23:10:14 +0530

    UC9 completed

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,75 +1,35 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
 
-//class LinkedList{
-//    Node head= null;
-//    class Node{
-//        char data;
-//        Node next;
-//        public  Node(char data){
-//            this.data = data;
-//            this.next=null;
-//        }
-//
-//    }
-//    public void add(char data){
-//        Node newNode = new Node(data);
-//        if(head==null){
-//            head=newNode;
-//            return;
-//        }
-//        Node currNode=head;
-//        while(currNode.next!=null){
-//            currNode=currNode.next;
-//        }
-//        currNode=newNode;
-//    }
-//    public void printAll(){
-//        Node currNode=head;
-//        while(currNode!=null){
-//            System.out.print(currNode.data+" ");
-//            currNode=currNode.next;
-//        }
-//    }
-//
-//
-//
-//}
+
 public class PalindromeCheckerApp {
 
     /**
      * Application entry point
      * This is the first method executed by the JVM
-     *when the program starts.
+     * when the program starts.
      * * @param args command-line arguments
+     *
      * @author samir dhakal
      * @version 1.0
-
      */
-
+    public static boolean isPalindrome(String str , int start , int end ) {
+        boolean isPalindrome = true;
+        if(str.charAt(start)!=str.charAt(end)) {
+            isPalindrome = false;
+        }
+        if(start>=end){
+            return true;
+        }
+        isPalindrome(str,start+1,end-1);
+        return isPalindrome;
+    }
     public static void main(String[] args) {
 
 
-        String orginalString = "mottom";
-        List<Character> list= new LinkedList<>();
-//        LinkedList list = new LinkedList();
-//
-//
-//
-for(char c : orginalString.toCharArray()) {
-    list.add(c);
-}
-//list.printAll();
+        String orginalString = "kaliiiii";
 
-        while(!list.isEmpty()) {
-            if (list.removeFirst()!=list.removeLast()) {
-                System.out.println(" The given string " + orginalString + " is not palindrome ");
-                return;
-            }
-            if(list.size()==1)
-                break;
-
-        }
-        System.out.println(" The given string " + orginalString + " is  palindrome ");
+        System.out.println(" The given string " + orginalString + "is palindrome :"+isPalindrome(orginalString,0,orginalString.length()-1));
 
     }
 }
